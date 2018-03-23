f3459ed1761fe548be81f884b7bae31920a09639
Sundararaju, Gopinath <gopinath.sundararaju@pearson.com>
2018-03-23 17:59:20 -0500

    7 Clustering servers

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-function doWork(duration) {
-    const start = Date.now();
-    while(Date.now() - start < duration) {        
-    }
+if (cluster.isMaster) {
+    // cause index.js to be executed again, but in child mode
+    cluster.fork();
+    cluster.fork();
+    cluster.fork();
 }
+else {
+    // I am a child. I am going to act normal 
+    const express = require('express');
+    const app = express();
 
-app.get('/',(req,res) => {
-    // This happens in event loop. It cannot do anything at all when this happens
-    doWork(5000);
-    res.send('Hi there!');
-});
+    function doWork(duration) {
+        const start = Date.now();
+        while (Date.now() - start < duration) {
+        }
+    }
 
-app.listen(3000);
+    app.get('/', (req, res) => {
+        // This happens in event loop. It cannot do anything at all when this happens
+        doWork(5000);
+        res.send('Hi there!');
+    });
+
+    app.get('/fast', (req, res) => {
+        res.send('This was fast');
+    });
+
+
+    app.listen(3000);
+}
