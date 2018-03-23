6a171fe25dfdadc632f0fc63315b4605d501fe30
Sundararaju, Gopinath <gopinath.sundararaju@pearson.com>
2018-03-23 17:41:56 -0500

    6 Blocking event loop

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 
+function doWork(duration) {
+    const start = Date.now();
+    while(Date.now() - start < duration) {        
+    }
+}
+
 app.get('/',(req,res) => {
+    // This happens in event loop. It cannot do anything at all when this happens
+    doWork(5000);
     res.send('Hi there!');
 });
 
